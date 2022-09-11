b54243ba46de62b0f7545f584626679d3d79f148
Franky <frankytchounkeu@gmail.com>
2022-09-11 15:18:09 -0700

    list of numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int chiffre = 0;
+while(chiffre < 10)
+{
+print("%d", chiffre);
+}
+printf("\n");
+return (0);
+}
