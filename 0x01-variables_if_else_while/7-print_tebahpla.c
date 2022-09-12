aed875ada54f5c7963e060faccd16ff13c55460a
Franky <frankytchounkeu@gmail.com>
2022-09-11 20:57:12 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+return (0);
+}
