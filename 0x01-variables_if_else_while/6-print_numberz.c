d9c30756f97e24fded5349565124b292aef26d13
Franky <frankytchounkeu@gmail.com>
2022-09-11 20:32:23 -0700

    single digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int single_digit = 0;
+while (single_digit <= 9)
+{
+putchar(single_digit);
+single_digit++;
+}
+putchar("\n");
+return(0);
+}
