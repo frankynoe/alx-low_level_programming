dc462929cbac86f2352f427d7296967b2ffcfc24
Franky <frankytchounkeu@gmail.com>
2022-09-11 21:18:31 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,11 +10,15 @@ int main(void)
 char digit = '0';
 char letter = 'a';
 while (digit <= '9')
+{
 putchar(digit);
 digit++;
+}
 while (letter <= 'f')
+{
 putchar(letter);
 letter++;
+}
 putchar('\n');
 return (0);
 }
