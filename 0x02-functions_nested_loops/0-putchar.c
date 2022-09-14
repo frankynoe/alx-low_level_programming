743504bb5fe7dd3f02bde707642c12673568c12f
Franky <frankytchounkeu@gmail.com>
2022-09-13 21:48:00 -0700

    prototype des fonctions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include main.h
+
+/**
+ *main - prints "_putchar on the screen"
+ *
+ *return: Always 0
+ */
+int main(void)
+{
+int i;
+char word[] = "_putchar";
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
+ _putchar('\n');
+return(0);
+}
